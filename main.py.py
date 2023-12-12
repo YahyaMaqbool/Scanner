0c1aa12fb67627d095ab0c02e2f3956b64a05de7
YahyaMaqbool <147329765+YahyaMaqbool@users.noreply.github.com>
2023-12-12 10:46:50 +0500

    Update main.py.py

--- main.py.py
+++ main.py.py
@@ -1,7 +1,7 @@
 # Task 1
 
 filename = "lexeme1.txt"
-user_input = "import maths\n\n def example_function():\n\n #this is a comment\n\n result=2+3\n\n return result"
+user_input = "import maths\n\n def area():\n\n # This is a simple function that calculates the area of a rectangle\n\n  length=10\n\n width=4\n\n result=10*4\n\n return result"
 
 def save_data_to_file(filename, data):
     with open(filename, 'w') as file:
@@ -44,15 +44,18 @@ class Preprocessor:
             file.writelines(modified_lines)
 
     def preprocess_file(self):
-        self.remove_blank_lines()
-        self.remove_comments_statements()
-        self.remove_imports_annotations()
+        try:
+            self.remove_blank_lines()
+            self.remove_comments_statements()
+            self.remove_imports_annotations()
 
-        print(f"Processed file saved to {self.output_file}")
-        print()
-        with open(self.output_file, 'r') as file:
-            data = file.read()
-            print("TASK 1 OUTPUT IS: \n" + data)
+            print(f"Processed file saved to {self.output_file}")
+            print()
+            with open(self.output_file, 'r') as file:
+                data = file.read()
+                print("TASK 1 OUTPUT IS: \n" + data)
+        except:
+            print(f"Error during preprocessing")
 
 # Run Task 1
 processor = Preprocessor(input_file, output_file)
@@ -66,26 +69,28 @@ class Processor:
         self.output_file = output_file
 
     def break_data_in_characters(self):
-        with open(self.input_file, 'r') as input_file:
-            buffer = []
-            while True:
-                char = input_file.read(1)
+        try:
+            with open(self.input_file, 'r') as input_file:
+                buffer = []
+                while True:
+                    char = input_file.read(1)
 
-                if not char:
-                    break
+                    if not char:
+                        break
 
-                if char != '\n':
-                    buffer.append(char)
+                    if char != '\n':
+                        buffer.append(char)
 
-            buffer.append('$')
+                buffer.append('$')
 
-        with open(self.output_file, 'w') as output_file:
-            output_file.write(''.join(buffer))
+            with open(self.output_file, 'w') as output_file:
+                output_file.write(''.join(buffer))
 
-        with open(self.output_file, 'r') as output_file:
-            data = output_file.read()
-            print("TASK 2 OUTPUT \n" + data) 
-
+            with open(self.output_file, 'r') as output_file:
+                data = output_file.read()
+                print("TASK 2 OUTPUT \n" + data)
+        except:
+            print(f"Error during processing")
 # Run Task 2
 input_file2 = "lexeme2.txt"
 output_file2 = "lexeme3.txt"
@@ -102,29 +107,28 @@ class LexicalAnalysis:
         self.output_file = output_file
 
     def generate_lexemes(self):
-        with open(self.input_file, 'r') as file:
-            content = file.read()
+        try:
+            with open(self.input_file, 'r') as file:
+                content = file.read()
 
-        keyword_pattern = r'\b(?:and|as|assert|break|class|continue|def|del|elif|else|except|False|finally|for|from|global|if|import|in|is|lambda|None|nonlocal|not|or|pass|raise|return|True|try|while|with|yield)\b'
-        identifier_pattern = r'\b[a-zA-Z_]\w*\b'
-        operator_pattern = r'\+|-|\*|\/|%|=|==|!=|<|>|<=|>=|and|or|not|in|is'
-        punctuator_pattern = r'{|}|\[|\]|\(|\)|,|;|:|\.'
-        literal_pattern = r'\b(?:\d+\.?\d*|".*?"|\'.*?\')\b'
+            keyword_pattern = r'\b(?:and|as|assert|break|class|continue|def|del|elif|else|except|False|finally|for|from|global|if|import|in|is|lambda|None|nonlocal|not|or|pass|raise|return|True|try|while|with|yield)\b'
+            identifier_pattern = r'\b[a-zA-Z_]\w*\b'
+            operator_pattern = r'\+|-|\*|\/|%|=|==|!=|<|>|<=|>=|and|or|not|in|is'
+            punctuator_pattern = r'{|}|\[|\]|\(|\)|,|;|:|\.'
+            literal_pattern = r'\b(?:\d+\.?\d*|".*?"|\'.*?\')\b'
 
-        combined_pattern = re.compile(f'({keyword_pattern}|{identifier_pattern}|{operator_pattern}|{punctuator_pattern}|{literal_pattern})', re.DOTALL)
+            combined_pattern = re.compile(f'({keyword_pattern}|{identifier_pattern}|{operator_pattern}|{punctuator_pattern}|{literal_pattern})', re.DOTALL)
 
-        lexemes = []
-        for token in combined_pattern.findall(content):
-            if token.strip():
-                lexemes.append(token)
+            lexemes = [token for token in combined_pattern.findall(content) if token.strip()]
 
-        with open(self.output_file, 'w') as file:
-            for lexeme in lexemes:
-                file.write("Lexeme: " + lexeme + '\n')
+            with open(self.output_file, 'w') as file:
+                file.write("\n".join(f"Lexeme: {lexeme}" for lexeme in lexemes))
 
-        with open(self.output_file, 'r') as file:
-            data = file.read()
-            print("Task 3 output is: \n" + data)
+            with open(self.output_file, 'r') as file:
+                data = file.read()
+                print("Task 3 output is: \n" + data)
+        except:
+            print(f"Error during lexical analysis")
 
 # Run Task 3
 input_file3 = "lexeme3.txt"
